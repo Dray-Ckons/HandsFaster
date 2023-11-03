4e9505914e67782c8584ac27b1486bb72fbe2953
Dray-Ckons <drayckons14@gmail.com>
2023-11-02 21:51:01 -0300

    Version 3.0.1 (falta ajustar un poco mas la deteccion de gestos) (se actualizo la ventana "sobre nosotros")

--- app/src/main/java/com/example/handsfaster/AboutActivity2.kt
+++ app/src/main/java/com/example/handsfaster/AboutActivity2.kt
@@ -15,8 +15,18 @@ class AboutActivity2 : AppCompatActivity() {
             "y la implementación de dos efectos de sonido en los toques que se ejecutan tocando la mitad superior de la pantalla (punto bueno) y la parte inferior (punto malo o error), y por ultimo relacionado al audio"+
             " es un aumento de velocidad de 5% cada 15 segundos en la canción de fondo con un máximo de aumento de 8 veces ósea la canción como máximo sonara al X1.8 de la velocidad original\n\n"+
 
-            "- la detección y reconocimiento de gestos a través del GestureDetecter"
+            "- la detección y reconocimiento de gestos a través del GestureDetecter\n\n"+
 
+            "Descripción de la versión 3:\n\n" +
+
+            "Se implementaron los gestos dentro de la pantalla principal de juego -MainActivity- , dentro de esta además se implemento un"+
+            " contador de errores interno que al fallar 5 veces con los gestos se tenga que reiniciar el nivel, faltaría hacer una actividad o pantalla de perdiste quieres "+
+            "reiniciar para que se vea mas estético\n" +
+            "\n" +
+            "\n" +
+            "adicional se implemento dentro de la clase de -borrador- mencionada en la versión anterior, un contador de -agitadas del celular- usando "+
+            "el acelerómetro, este cuenta el movimiento en el eje X si varia en positivo o negativo en 8 unidades y luego lo vuelve a hacer en el valor contrario"+
+            " ósea +8 con -8 y -8 con +8 para recién contarlo como una sola sacudida"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about2)
